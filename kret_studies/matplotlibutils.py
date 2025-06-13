1b9f0203b5596c2019d0ec8612a2f2d1ab50687b
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-06-13 11:26:35 -0400

    Columbia stat2

--- kret_studies/matplotlibutils.py
+++ kret_studies/matplotlibutils.py
@@ -4,20 +4,62 @@ import typing as t
 
 import matplotlib.pyplot as plt
 from matplotlib.axes import Axes
+from matplotlib.figure import Figure
 
 from .typed_cls import Subplots_TypedDict
+from .numpy_utils import SingleReturnArray
+
+
+@t.overload
+def subplots(  # type: ignore
+    ncols: t.Literal[1] = ...,
+    nrows: t.Literal[1] = ...,
+    width_per: float = ...,
+    height_per: float = ...,
+    **subplot_args: t.Unpack[Subplots_TypedDict],
+) -> t.Tuple[Figure, Axes]: ...
+
+
+@t.overload
+def subplots(  # type: ignore
+    ncols: int = ...,
+    nrows: t.Literal[1] = ...,
+    width_per: float = ...,
+    height_per: float = ...,
+    **subplot_args: t.Unpack[Subplots_TypedDict],
+) -> t.Tuple[Figure, SingleReturnArray[Axes]]: ...
+
+
+@t.overload
+def subplots(  # type: ignore
+    ncols: t.Literal[1] = ...,
+    nrows: int = ...,
+    width_per: float = ...,
+    height_per: float = ...,
+    **subplot_args: t.Unpack[Subplots_TypedDict],
+) -> t.Tuple[Figure, SingleReturnArray[Axes]]: ...
+
+
+@t.overload
+def subplots(
+    ncols: int = ...,
+    nrows: int = ...,
+    width_per: float = ...,
+    height_per: float = ...,
+    **subplot_args: t.Unpack[Subplots_TypedDict],
+) -> t.Tuple[Figure, SingleReturnArray[SingleReturnArray[Axes]]]: ...
 
 
 def subplots(
-    nrows: int = 1,
     ncols: int = 1,
-    width_per: float = 6,
+    nrows: int = 1,
+    width_per: float = 8,
     height_per: float = 6,
-    **kwargs: t.Unpack[Subplots_TypedDict],
+    **subplot_args: t.Unpack[Subplots_TypedDict],
 ):
     fig_width = ncols * width_per
     fig_height = nrows * height_per
-    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height))
+    fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width, fig_height), **subplot_args)
     plt.close(fig)
     return fig, ax
 
--- kret_studies/notebook_imports.py
+++ kret_studies/notebook_imports.py
@@ -5,11 +5,20 @@ import re
 import sys
 import typing as t
 from pathlib import Path
-
+from IPython.display import display, HTML
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import polars as pl
 import seaborn as sns
+import statsmodels.formula.api as smf
+import scipy.stats as stats  # For statistical tests and plots
 
+# region sklearn
+import sklearn
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error
+
+# endregion
 sns.set_theme()
