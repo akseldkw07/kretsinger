172cc1554dd7ff794ec0d21f946088f0b30d059e
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-10-08 18:44:21 -0400

    uks-display Modified: - backup_log.txt - __init__.py - display_utils.py

--- kret_studies/__init__.py
+++ kret_studies/__init__.py
@@ -18,3 +18,6 @@ import kret_studies.date_utils as uks_date
 import kret_studies.kret_pandas as uks_pd
 import kret_studies.kret_sklearn as uks_sklearn
 import kret_studies.kret_torch as uks_torch
+
+"""Commonly used functions"""
+from kret_studies.display_utils import dataset_to_table as dtt
