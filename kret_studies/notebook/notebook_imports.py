5d843f30fbcd5787bdefd4f1c4b6b080967cc38d
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2025-11-14 12:32:14 -0500

    MNIST-99p Added: - mnist.ipynb - mnist.pdf Modified: - backup_log.txt - abc_nn.py - base_nn.py - notebook_imports.py - classification_example.ipynb Deleted: - classification_example.pdf

--- kret_studies/notebook/notebook_imports.py
+++ kret_studies/notebook/notebook_imports.py
@@ -75,6 +75,9 @@ import pytensor.tensor as pt"""
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data.dataset import Subset
 
 # numpy, pandas
 import numpy as np
