74679ebb63af34a473936fe4281f98eba16ff226
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2026-01-06 14:36:31 -0500

    import-analyzer

---
+++ kret_notebook/constants_FINAL.py
@@ -0,0 +1,8 @@
+from kret_utils.constants_kret import KretConstants
+from kret_lightning.constants_lightning import LightningConstants
+from kret_torch_utils.constants_torch import TorchConstants
+
+class KretNotebookPaths:
+    DATA_DIR = KretConstants.DATA_DIR
+    LIGHTNING_LOG_DIR = LightningConstants.LIGHTNING_LOG_DIR
+    
