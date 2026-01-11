a73f529c4dbbee33da0f036a18f7daf27325eb42
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2026-01-10 19:33:17 -0500

    lgb-save-load

--- kret_lgbm/lgbm_nb_imports.py
+++ kret_lgbm/lgbm_nb_imports.py
@@ -8,6 +8,7 @@ from lightgbm import Dataset, Dataset as DatasetLGBM, LGBMClassifier, LGBMRegres
 from lightgbm.callback import early_stopping, log_evaluation
 
 from .constants_lgbm import LGBM_Constants, LGBM_Defaults
+import joblib
 
 start_time_end = time.time()
 print(f"[{__name__}] Imported {__name__} in {start_time_end - start_time:.4f} seconds")
