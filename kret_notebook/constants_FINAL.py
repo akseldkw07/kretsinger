1297a54710c998491344372eb19e064c35a832bb
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2026-01-18 23:45:37 +0900

    lightning-metrics

--- kret_notebook/constants_FINAL.py
+++ kret_notebook/constants_FINAL.py
@@ -1,6 +1,7 @@
 from kret_lgbm.constants_lgbm import LGBM_Constants, LGBM_Defaults
 from kret_lightning.constants_lightning import LightningConstants
 from kret_matplotlib.constants_mpl import MPLConstants, MPLDefaults
+from kret_np_pd.constants_np_pd import NP_PD_Defaults
 from kret_polars.constants_polars import PolarsConstants
 from kret_rosetta.constants_rosetta import RosettaConstants
 from kret_sklearn.constants_sklearn import SklearnDefaults
@@ -21,4 +22,4 @@ class KretConstantsNB(
 ): ...
 
 
-class KretDefaultsNB(MPLDefaults, SklearnDefaults, LGBM_Defaults): ...
+class KretDefaultsNB(MPLDefaults, SklearnDefaults, LGBM_Defaults, NP_PD_Defaults): ...
