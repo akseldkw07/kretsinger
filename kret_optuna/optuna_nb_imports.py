ce37f98d618dc5aacb0acd9e091b0778eab0e87e
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2026-01-18 19:21:55 -0500

    Optuna

---
+++ kret_optuna/optuna_nb_imports.py
@@ -0,0 +1,9 @@
+# autoflake: skip_file
+import time
+
+start_time = time.time()
+
+import optuna
+
+start_time_end = time.time()
+print(f"[{__name__}] Imported {__name__} in {start_time_end - start_time:.4f} seconds")
