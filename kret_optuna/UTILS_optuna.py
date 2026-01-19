0a959220f51d13e274492c8d4da581b6dcf77ced
Aksel Kretsinger-Walters <46535914+akseldkw07@users.noreply.github.com>
2026-01-18 19:43:16 -0500

    Optuna-0 (#69)

---
+++ kret_optuna/UTILS_optuna.py
@@ -0,0 +1,5 @@
+from .constants_optuna import OptunaDefaults
+
+
+class KRET_OPTUNA_UTILS(OptunaDefaults):
+    pass
---
+++ kret_optuna/constants_optuna.py
@@ -0,0 +1,9 @@
+import typing as t
+
+from .typed_cls_optuna import *
+
+
+class OptunaDefaults: ...
+
+
+class OptunaConstants: ...
