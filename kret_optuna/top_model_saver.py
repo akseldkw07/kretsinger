5be6e83dd41ee5dc0e768f31aa5a1f9b0a2a66b2
Kretsinger-Walters, Aksel <akseldkw@gmail.com>
2026-01-29 13:58:15 -0500

    optuna-model-saver-done Modified: - backup_log.txt - abc_lightning.py - base_lightning_nn.py - top_model_saver.py

--- kret_optuna/top_model_saver.py
+++ kret_optuna/top_model_saver.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import lightning as L
 import optuna
 from lightning.pytorch import LightningModule
+from lightning.pytorch.core.saving import save_hparams_to_yaml
 
 
 class TopNModelSaver:
@@ -45,14 +46,26 @@ class TopNModelSaver:
         return False
 
     def _save(self, trainer: L.Trainer, model: LightningModule, score: float, trial: optuna.trial.Trial):
-        path = self.save_dir / f"trial_{trial.number:04d}_score_{score:.4f}.ckpt"
-        trainer.save_checkpoint(path, weights_only=False)
-        self._leaderboard.append((score, trial.number, path))
+        stem = f"trial_{trial.number:04d}_score_{score:.4f}"
+        ckpt_path = self.save_dir / f"{stem}.ckpt"
+        yaml_path = self.save_dir / f"{stem}.hparams.yaml"
+
+        trainer.save_checkpoint(ckpt_path, weights_only=False)
+        save_hparams_to_yaml(yaml_path, dict(model.hparams_initial))
+
+        self._leaderboard.append((score, trial.number, ckpt_path))
+
+    @staticmethod
+    def _yaml_path_for(ckpt_path: Path) -> Path:
+        """trial_0001_score_0.6989.ckpt -> trial_0001_score_0.6989.hparams.yaml"""
+        # Can't use .with_suffix() because the score contains a dot
+        return ckpt_path.parent / (ckpt_path.name.removesuffix(".ckpt") + ".hparams.yaml")
 
     def _evict_worst(self):
         worst = self._worst_entry
         self._leaderboard.remove(worst)
         worst[2].unlink(missing_ok=True)  # delete checkpoint file
+        self._yaml_path_for(worst[2]).unlink(missing_ok=True)
 
     @property
     def best_checkpoints(self) -> list[tuple[float, int, Path]]:
